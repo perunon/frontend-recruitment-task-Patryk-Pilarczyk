d33b31115996d2494a561b7c892588acb5f2814b
perunon <patryk.pilarczyk.00@gmail.com>
2022-06-08 18:35:59 +0200

    Added _resets.scss file and set sass (Dart-sass) as sass compiler due to issue with gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,46 +9,40 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 var replace = require('gulp-replace');
 
+//Set sass compiler to sass (Dart-sass)
+sass.compiler = require('sass');
+
 // File paths
-const files = { 
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
+const files = {
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js',
 };
 
-function scssTask(){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass([])) // compile SCSS to CSS
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(dest('dist')); // put final CSS in dist folder
 }
 
-function jsTask(){
-    return src([
-        files.jsPath
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(dest('dist')
-    );
+function jsTask() {
+  return src([files.jsPath])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'));
 }
 
 var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
+function cacheBustTask() {
+  return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'));
 }
 
-function watchTask(){
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));    
+function watchTask() {
+  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
 }
 
-exports.default = series(
-    parallel(scssTask, jsTask), 
-    cacheBustTask,
-    watchTask
-);
+exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
